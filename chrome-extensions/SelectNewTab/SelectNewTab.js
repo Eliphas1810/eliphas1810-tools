e0640c3f41c62e13a33c1d472f94e8f39e924ebc
Eliphas1810 <152246411+Eliphas1810@users.noreply.github.com>
2024-01-18 14:40:31 +0900

    bug fixed for id duplicate error.

--- chrome-extensions/SelectNewTab/SelectNewTab.js
+++ chrome-extensions/SelectNewTab/SelectNewTab.js
@@ -1,17 +1,23 @@
-var selectNewTabId = "SelectNewTab";
+//マウスの右クリック メニューに、この拡張機能が追加した自作のメニューが存在する場合は、それだけを削除
+//
+//これを起動しないと、Chromeを閉じてから再び開いた場合に、自作のメニューの追加の重複エラーが発生してしまいます。
+//
+//chrome.runtime.onInstalled.addListener(() => {});を利用すると、Chromeを閉じてから再び開いた場合に、この拡張機能が起動されないので、自作のメニューは存在するが、動作しなく成ってしまいます。
+//
+chrome.contextMenus.removeAll();
 
-//マウスの右クリック メニューに追加
+//マウスの右クリック メニューに、自作のメニューを追加
 chrome.contextMenus.create({
-    id: selectNewTabId,
+    id: "SelectNewTab",
     title: "リンクを新しいタブで開いて選択状態にする",
     contexts: ["link"]
 });
 
-//マウスの右クリック メニューで自作のメニューがクリックして選択された時の処理を追加
+//マウスの右クリック メニューで、自作のメニューがクリックして選択された時の処理を追加
 chrome.contextMenus.onClicked.addListener((info, tab) => {
 
-    //マウスの右クリック メニューで自作のメニューがクリックして選択された場合
-    if (info.menuItemId == selectNewTabId) {
+    //マウスの右クリック メニューで、自作のメニューがクリックして選択された場合
+    if (info.menuItemId == "SelectNewTab") {
 
         //リンクを新しいタブで開いて選択状態にします。
         chrome.tabs.create({url: info.linkUrl, selected: true});
